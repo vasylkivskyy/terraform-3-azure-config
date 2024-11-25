9042101ab3407f2289234a350aa064e2c8b1a93b
Volodymyr Vasylkivskyi <volodymyr.vasylkivskyi@wickes.co.uk>
2024-11-25 16:29:02 +0200

    add description to outputs

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,11 +1,14 @@
 output "rg_id" {
-  value = data.azurerm_resource_group.existing.id
+  description = "The ID of the resource group"
+  value       = data.azurerm_resource_group.existing.id
 }
 
 output "sa_blob_endpoint" {
-  value = azurerm_storage_account.sa.primary_blob_endpoint
+  description = "The endpoint URL for the blob storage"
+  value       = azurerm_storage_account.sa.primary_blob_endpoint
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  description = "The ID of the virtual network"
+  value       = azurerm_virtual_network.vnet.id
 }
